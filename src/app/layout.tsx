4d71813c47411b0e632749a3a71d7fec7dcfb463
suman mishra <suman132j@gmail.com>
2024-06-19 23:36:39 +0530

    magic ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="NoSidebar">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss"
 
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
 const config = {
   darkMode: ["class"],
   content: [
@@ -34,7 +39,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")]
 } satisfies Config
 
 export default config
